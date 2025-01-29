c3afb4cfda153099f279cfe55b64e6dd8373231a
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-01-29 12:52:10 +0000

    Adding config file for db connection

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # HouseworkTracker
+
+# Setup
+
+Add a .env file in the root directory. This needs to contain the following data (replacing with the correct details):
+POSTGRES_USER=user
+POSTGRES_PASSWORD=password
+POSTGRES_SERVER=server
+POSTGRES_PORT=1234
+POSTGRES_DB=housework
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from config import settings
 
 app = FastAPI()
+
+print(settings.DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 @app.get("/")
 def main():
+    print(engine)
+    print(SessionLocal)
     return {"message": "Hello, World!"}
