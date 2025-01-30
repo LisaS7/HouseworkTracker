8f31aa2401d60776a7b8ea458968b935b4702346
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-01-30 15:16:37 +0000

    Adding tests and validation

--- DB/Task.py
+++ DB/Task.py
@@ -1,8 +1,10 @@
 import enum
+import datetime as dt
 from sqlalchemy import Column, Integer, String, Boolean, Date, ForeignKey, Enum
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.orm import relationship, validates
 
 from DB.session import Base
+from config import settings
 
 
 class Priority(enum.Enum):
@@ -15,10 +17,36 @@ class Task(Base):
     __tablename__ = "tasks"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    title = Column(String, nullable=False)
+    title = Column(String(settings.MAX_TITLE_LENGTH), nullable=False)
     priority = Column(Enum(Priority), default=Priority.LOW)
     due_date = Column(Date)
     complete = Column(Boolean, default=False)
 
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     user = relationship("User", back_populates="tasks")
+
+    @validates("title")
+    def validate_title_length(self, _, value):
+        if len(value) > settings.MAX_TITLE_LENGTH:
+            raise ValueError(
+                f"Title cannot exceed {settings.MAX_TITLE_LENGTH} characters. Provided: {len(value)}"
+            )
+        return value
+
+    @validates("priority")
+    def validate_priority(self, _, value):
+        if value not in Priority:
+            raise ValueError(f"{value} is not a valid priority")
+
+    @validates("due_date")
+    def validate_due_date(self, _, value):
+        if isinstance(value, str):
+            try:
+                return dt.datetime.strptime(value, "%Y-%m-%d").date()
+            except ValueError:
+                raise ValueError(
+                    f"Date is invalid, you provided {value} of type {type(value)}"
+                )
+
+        if isinstance(value, dt.date):
+            return value
--- Tests/test_models.py
+++ Tests/test_models.py
@@ -1,15 +1,21 @@
 import pytest
+from datetime import date
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError
 
 
-from DB.Task import Task
+from DB.Task import Task, Priority
 from DB.User import User
 from DB.session import Base
+from config import settings
 
 
 engine = create_engine("sqlite:///:memory:", echo=True)
 Session = sessionmaker(bind=engine)
+
+
+# ---------- FIXTURES ----------
 
 
 @pytest.fixture
@@ -36,6 +42,9 @@ def test_user(db):
     return user
 
 
+# ---------- TASK ----------
+
+
 def test_create_task(test_user, db):
     task = Task(title="Mop kitchen", user_id=test_user.id)
     db.add(task)
@@ -45,3 +54,74 @@ def test_create_task(test_user, db):
 
     assert retrieved_task is not None
     assert retrieved_task.user_id == test_user.id
+
+
+def test_no_title(test_user, db):
+    task = Task(user_id=test_user.id)
+    db.add(task)
+
+    with pytest.raises(IntegrityError):
+        db.commit()
+
+
+def test_long_title(test_user):
+    max_length = settings.MAX_TITLE_LENGTH
+    test_length = max_length + 10
+    long_title = "A" * (test_length)
+
+    with pytest.raises(
+        ValueError,
+        match=f"Title cannot exceed {max_length} characters. Provided: {test_length}",
+    ):
+        task = Task(title=long_title, user_id=test_user.id)
+
+
+def test_default_priority(test_user, db):
+    task = Task(title="Test Priority", user_id=test_user.id)
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+    retrieved_task = db.query(Task).filter_by(title="Test Priority").first()
+
+    assert retrieved_task.priority == Priority.LOW
+
+
+def test_invalid_priority(test_user):
+    invalid_word = "Banana"
+    with pytest.raises(ValueError, match=f"{invalid_word} is not a valid priority"):
+        task = Task(title="Test Priority", priority=invalid_word, user_id=test_user.id)
+
+
+def test_due_date(test_user, db):
+    due_date = date(2025, 1, 1)
+    task = Task(title="Test Date", user_id=test_user.id, due_date=due_date)
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+    retrieved_task = db.query(Task).filter_by(title="Test Date").first()
+
+    assert retrieved_task.due_date == due_date
+
+
+def test_invalid_date(test_user, db):
+    due_date = "Banana"
+    with pytest.raises(
+        ValueError, match="Date is invalid, you provided Banana of type <class 'str'>"
+    ):
+        task = Task(title="Test Date", user_id=test_user.id, due_date=due_date)
+
+
+# ---------- USER ----------
+
+
+def test_default_active(db):
+    pass
+
+
+def test_unique_email(db):
+    pass
+
+
+# ---------- RELATIONSHIP ----------
+def test_get_tasks_for_user(db):
+    pass
