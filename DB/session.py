5060ca6bc19498c65dcf03f3d3e71f2731a10085
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-01-31 13:02:35 +0000

    Adding get_db function for dependency injection

--- DB/session.py
+++ DB/session.py
@@ -1,8 +1,20 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import Session, sessionmaker, declarative_base
 from config import settings
+from typing import Generator
+
 
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+# This function grabs a fresh db connection.
+# It closes the connection once it is no longer needed.
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
