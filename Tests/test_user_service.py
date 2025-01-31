f118e64f329ed966eab997a4321061b661d46a00
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-01-31 13:42:43 +0000

    Adding custom exception and update user function

--- Tests/test_user_service.py
+++ Tests/test_user_service.py
@@ -44,8 +44,29 @@ def test_get_all_users(test_users, db):
     assert users[1].name == "Dwight"
 
 
+def test_get_user_by_id(test_users, db):
+    user = get_user_by_id(db, 1)
+    assert user.name == "Michael"
+
+
+def test_user_id_invalid(test_users, db):
+    with pytest.raises(UserNotFoundException, match="User not found for id 5"):
+        get_user_by_id(db, 5)
+
+
 def test_create_user(db):
     user = User(name="Pam", email="pam@dundermifflin.com")
     created_user = create_user(db, user)
     retrieved_user = db.query(User).filter_by(name="Pam").first()
     assert retrieved_user == created_user
+
+
+def test_update_user(test_users, db):
+    update_user(db, 1, {"name": "Creed"})
+    retrieved_user = db.query(User).filter(User.id == 1).first()
+    assert retrieved_user.name == "Creed"
+    assert retrieved_user.email == "michael@dundermifflin.com"
+
+
+def test_delete_user(test_users, db):
+    pass
--- services/User.py
+++ services/User.py
@@ -4,6 +4,13 @@ from models.User import User
 from typing import List
 
 
+class UserNotFoundException(Exception):
+    def __init__(self, id: int):
+        self.message = f"User not found for id {id}"
+        self.status_code = 404
+        super().__init__(self.message)
+
+
 class UserModel(BaseModel):
     id: int
     name: str
@@ -26,8 +33,11 @@ def get_all_users(db: Session) -> List[User]:
     return db.query(User).all()
 
 
-def get_user_by_id():
-    pass
+def get_user_by_id(db: Session, id: int) -> User:
+    user = db.query(User).filter(User.id == id).first()
+    if not user:
+        raise UserNotFoundException(id)
+    return user
 
 
 def create_user(db: Session, user: UserCreate) -> User:
@@ -37,9 +47,17 @@ def create_user(db: Session, user: UserCreate) -> User:
     return user
 
 
-def update_user():
-    pass
+def update_user(db: Session, id: int, user: UserUpdate) -> User:
+    existing_user = get_user_by_id(db, id)
+
+    for key, value in user.items():
+        setattr(existing_user, key, value)
+
+    db.commit()
+    db.refresh(existing_user)
+
+    return existing_user
 
 
-def delete_user():
-    pass
+def delete_user(db: Session, id: int):
+    existing_user = get_user_by_id(db, id)
