dae4ca8002537655027924436ea25eaff33a9e49
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-02-02 19:58:57 +0000

    Fixing problem with model import order

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 
 from DB.session import database
 from config import templates, PROJECT_NAME, PROJECT_VERSION
 from routes import users
 
-from seed import seed
+# These imports aren't used here but importing them ensures the models are defined in the correct order
+from models.Task import Task
+from models.Tag import Tag
+from models.User import User
 
 
 app = FastAPI(title=PROJECT_NAME, version=PROJECT_VERSION)
@@ -12,10 +16,9 @@ app = FastAPI(title=PROJECT_NAME, version=PROJECT_VERSION)
 
 app.include_router(users.router, prefix="/users", tags=["users"])
 
-
-# seed()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
-def main(request: Request):
+def home(request: Request):
     return templates.TemplateResponse(request=request, name="home.jinja")
