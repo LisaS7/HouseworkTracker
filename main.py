0419a849d4b6690e8019c307da90048211950cff
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-02-03 17:54:16 +0000

    Adding tasks router

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.staticfiles import StaticFiles
 
 from DB.session import database
 from config import templates, PROJECT_NAME, PROJECT_VERSION, TESTING
-from routes import users
+from routes import users, tasks
 
 # These imports aren't used here but importing them ensures the models are defined in the correct order
 from models.Task import Task
@@ -16,6 +16,8 @@ from scripts import seed
 app = FastAPI(title=PROJECT_NAME, version=PROJECT_VERSION)
 
 app.include_router(users.router, prefix="/users", tags=["users"])
+app.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
+
 
 if not TESTING:
     app.mount("/static", StaticFiles(directory="static"), name="static")
