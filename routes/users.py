e02b6f7c922df7237cc89442d4d7173b14a42fb7
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-02-05 13:09:23 +0000

    Refactoring

--- routes/users.py
+++ routes/users.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Request, Depends
-from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from config import templates, logger
 from DB.session import get_db
@@ -12,13 +11,8 @@ router = APIRouter()
 async def all_users(request: Request, db: Session = Depends(get_db)):
     data = get_all_users(db)
 
-    accept_header = request.headers.get("Accept", "")
-
-    if "application/json" in accept_header:
-        return JSONResponse(content=data)
-
     logger.info(f"{request.method} {request.url}")
 
     return templates.TemplateResponse(
-        "users.html", context={"request": request, "users": data}
+        "/users/users.html", context={"request": request, "users": data}
     )
